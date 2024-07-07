8568a83eee6434b1272b556cb64c9c162880763b
Areeba bashir <areebabashir333@gmail.com>
2024-07-07 23:24:44 +0500

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from "../component/Layout/Layout"
+const HomePage = () => {
+    return (
+        <Layout title={"best offer- Ecommerce" }>
+            <h1>Home page</h1>
+        </Layout>
+    )
+}
+
+export default HomePage
