c930b0d75c54c39a9b521d5987d71ba9f31e5a72
Areeba bashir <areebabashir333@gmail.com>
2024-07-12 12:30:21 +0500

    chnges

--- src/pages/Admin/AdminDashboard2.js
+++ src/pages/Admin/AdminDashboard2.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
-const AdminDashboard2= () => {
+import React from "react";
+import AdminMenu from "../../component/Layout/AdminMenu";
+import Layout from "../../component/Layout/Layout";
+import { useAuth } from "../../context/auth";
+const AdminDashboard2 = () => {
+    const [auth] = useAuth();
     return (
-        <div>AdminDashboard</div>
-    )
-}
+        <Layout>
+            <div className="container-fluid m-3 p-3">
+                <div className="row">
+                    {" "}
+                    <div className="col-md-3">
+                        <AdminMenu />
+                    </div>
+                    <div className="col-md-9">
+                        <div className="card w-75 p-3">
+                            <h3> Admin Name : {auth?.user?.name}</h3>
+                            <h3> Admin Email : {auth?.user?.email}</h3>
+                            <h3> Admin Contact : {auth?.user?.phone}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
-export default AdminDashboard2
+export default AdminDashboard2;
